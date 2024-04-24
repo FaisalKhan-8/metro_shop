44082aab682d12c57940f8cb8f2588900aaa3d65
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-04-25 01:21:50 +0530

    basics setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/customs/theme-provider';
+import Header from '@/components/customs/Header';
+import Footer from '@/components/customs/Footer';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -26,7 +28,11 @@ export default function RootLayout({
           defaultTheme='system'
           enableSystem
           disableTransitionOnChange>
-          {children}
+          <div className='flex flex-col min-h-screen'>
+            <Header />
+            <main className='flex-grow'>{children}</main>
+            <Footer />
+          </div>
         </ThemeProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main className=''>Hello</main>;
+  return <div className=''>Hello</div>;
 }
